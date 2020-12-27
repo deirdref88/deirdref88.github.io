41f1b74624fa8a8189a86f4dd0a0c1440e4c1c60
Deirdre <deirdref88@hotmail.com>
2020-12-26 18:17:52 -0600

    pushing to heroku

--- application.py
+++ application.py
@@ -8,10 +8,10 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 # Used helpers.py from Finance pset
 from helpers import apology, login_required
-from urllib.parse import urlparse
+from urllib.parse
 import psycopg2
-urlparse.uses_netloc.append("postgres")
-url = urlparse(os.environ["DATABASE_URL"])
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
 conn = psycopg2.connect(
  database=url.path[1:],
  user=url.username,
