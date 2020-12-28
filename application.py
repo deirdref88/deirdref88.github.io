b4c736a3af77f7371830a42990cf110ce3924e87
CJYang-Yale <70782400+CJYang-Yale@users.noreply.github.com>
2020-12-27 19:00:56 -0500

    Update application.py

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Cache-Control"] = "max-age=90000"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
